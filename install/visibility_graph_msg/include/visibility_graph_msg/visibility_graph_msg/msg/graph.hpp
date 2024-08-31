25838532c6605be73025134470cac6e18833016d
Mohamed Elsayed <40526301+DevilManiac@users.noreply.github.com>
2024-08-31 23:36:40 +0800

    Finished translating now testing

---
+++ install/visibility_graph_msg/include/visibility_graph_msg/visibility_graph_msg/msg/graph.hpp
@@ -0,0 +1,11 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef VISIBILITY_GRAPH_MSG__MSG__GRAPH_HPP_
+#define VISIBILITY_GRAPH_MSG__MSG__GRAPH_HPP_
+
+#include "visibility_graph_msg/msg/detail/graph__struct.hpp"
+#include "visibility_graph_msg/msg/detail/graph__builder.hpp"
+#include "visibility_graph_msg/msg/detail/graph__traits.hpp"
+
+#endif  // VISIBILITY_GRAPH_MSG__MSG__GRAPH_HPP_
---
+++ install/visibility_graph_msg/share/visibility_graph_msg/cmake/visibility_graph_msg__rosidl_typesupport_cppExport-noconfig.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file.
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "visibility_graph_msg::visibility_graph_msg__rosidl_typesupport_cpp" for configuration ""
+set_property(TARGET visibility_graph_msg::visibility_graph_msg__rosidl_typesupport_cpp APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(visibility_graph_msg::visibility_graph_msg__rosidl_typesupport_cpp PROPERTIES
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_NOCONFIG "rosidl_runtime_c::rosidl_runtime_c;rosidl_typesupport_cpp::rosidl_typesupport_cpp;rosidl_typesupport_c::rosidl_typesupport_c"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib/libvisibility_graph_msg__rosidl_typesupport_cpp.so"
+  IMPORTED_SONAME_NOCONFIG "libvisibility_graph_msg__rosidl_typesupport_cpp.so"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS visibility_graph_msg::visibility_graph_msg__rosidl_typesupport_cpp )
+list(APPEND _IMPORT_CHECK_FILES_FOR_visibility_graph_msg::visibility_graph_msg__rosidl_typesupport_cpp "${_IMPORT_PREFIX}/lib/libvisibility_graph_msg__rosidl_typesupport_cpp.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
